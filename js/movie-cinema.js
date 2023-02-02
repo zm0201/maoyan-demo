ffaab327e200af57247c157ba7d8d0cbe2f23109
zm_good_luck <zm_good_luck@sina.com>
2023-02-02 22:54:59 +0800

    fix: 修复滑块逻辑

--- js/movie-cinema.js
+++ js/movie-cinema.js
@@ -4,39 +4,47 @@ const docNodes = {
 };
 
 const events = {
-  handleTabsClick(event) {
-    // 获取真正被点击的DOM节点
-    const target = event.target;
-    // 判断这个节点是不是<li />
-    let node;
-    if (target.tagName.toLowerCase() === 'a') {
-      node = target.parentElement;
-    } else {
-      node = target;
-    }
-
-    // 取消上一个current类
-    const lastCurrent = document.querySelector(
-      '.tabs-container > .left > ul > li.current'
-    );
-    if (!lastCurrent) {
+  handleTabsClick: (function () {
+    // 获取滑块
+    const block = document.querySelector('.tabs-container > .tab-active-block');
+    if (!block) {
       return;
     }
-    lastCurrent.classList.remove('current');
-    // 给li设置current类
-    node.classList.add('current');
+    const { width: blockWidth, left: blockLeft } =
+      block.getBoundingClientRect();
 
-    // 获取li左侧和ul左侧之间的距离
-    const offsetLeft = node.offsetLeft;
-    console.log(node, offsetLeft);
-    // 获取li本身的宽度
-    const liWidth = node.getBoundingClientRect().width;
+    return function (event) {
+      // 获取真正被点击的DOM节点
+      const target = event.target;
+      // 判断这个节点是不是<li />
+      let node;
+      if (target.tagName.toLowerCase() === 'a') {
+        node = target.parentElement;
+      } else {
+        node = target;
+      }
 
-    block.style.transform = `translateX(${
-      offsetLeft + liWidth / 2 - blockWidth / 2
-      // blockLeft + liWidth / 2 - blockWidth / 2
-    }px)`;
-  },
+      // 取消上一个current类
+      const lastCurrent = document.querySelector(
+        '.tabs-container > .left > ul > li.current'
+      );
+      if (!lastCurrent) {
+        return;
+      }
+      lastCurrent.classList.remove('current');
+      // 给li设置current类
+      node.classList.add('current');
+
+      // 获取li左侧和ul左侧之间的距离
+      const offsetLeft = node.offsetLeft;
+      // 获取li本身的宽度
+      const liWidth = node.getBoundingClientRect().width;
+
+      block.style.transform = `translateX(${
+        offsetLeft + liWidth / 2 - blockWidth / 2
+      }px)`;
+    };
+  })(),
 };
 
 (function () {
